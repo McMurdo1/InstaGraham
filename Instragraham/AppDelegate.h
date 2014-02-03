868e5a8636e8d8c96eed7ee091ec08d69acf48eb
McMurdo1 <matt@1423ventures.com>
2014-02-03 15:16:56 -0600

    Initial Commit

---
+++ Instragraham/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Instragraham
+//
+//  Created by Matthew Graham on 2/3/14.
+//  Copyright (c) 2014 Matthew Graham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
